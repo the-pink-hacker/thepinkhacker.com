0526119c0b97165259b72e1c7a4c9b5f3e156a36
Ryan Garrett <RyanGar46@gmail.com>
2023-04-02 18:50:23 -0400

    Switch from webHashHistory to webHistory

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import "./style/main.scss";
 import App from "./App.vue";
 import Home from "./components/pages/Home.vue";
@@ -7,7 +7,7 @@ import Apollo from "./components/pages/projects/Apollo.vue";
 
 createApp(App)
     .use(createRouter({
-        history: createWebHashHistory(),
+        history: createWebHistory(),
         routes: [
             {
                 path: "/",
