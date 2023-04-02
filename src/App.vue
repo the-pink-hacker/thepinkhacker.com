4eb7e244b213417770caecdacdddb7a3d2125d92
Ryan Garrett <RyanGar46@gmail.com>
2023-04-02 03:14:40 -0400

    Add test counter

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
 </script>
 
 <template>
-  <p>This website is under heavy construction right now. Check back later.</p>
+  <div>
+    <p>This website is under heavy construction right now. Check back later.</p>
+    <button @click="count++">Counter: {{ count }}</button>
+  </div>
 </template>
 
 <style lang="scss" scoped>
