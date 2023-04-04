0744d0be1a9655c45a9ed64829fde8c3ecb6b69c
Ryan Garrett <RyanGar46@gmail.com>
2023-04-04 02:59:28 -0400

    Improve image selector menu animations

--- src/components/ImageSelectorMenuHorizontal.vue
+++ src/components/ImageSelectorMenuHorizontal.vue
@@ -53,28 +53,71 @@ function getSelectedClass(index: number) {
   user-select: none;
   overflow: hidden;
   transition: width var(--ui-move-time);
+  justify-content: center;
+  align-items: center;
 
   &.selected {
     width: 300%;
+
+    .image-title-wrapper {
+      min-height: fit-content;
+      min-width: fit-content;
+      height: 33%;
+      width: 33%;
+      padding: 8px;
+    }
+
+    .image-title {
+      font-size: var(--font-large-size);
+    }
+
+    &:hover .image-title::after {
+      width: 100%;
+    }
   }
 
-  &:hover .image-title-wrapper {
+  &:hover .image-title-wrapper,
+  &.selected .image-title-wrapper {
+    font-size: var(--font-large-size);
     opacity: 100%;
-    backdrop-filter: blur(var(--blur-size));
+    backdrop-filter: brightness(33%);
+    transition-duration: 500ms;
   }
 }
 
 .image-title-wrapper {
   opacity: 0;
   display: flex;
-  backdrop-filter: blur(0px);
-  transition: backdrop-filter 200ms;
+  backdrop-filter: brightness(100%);
+  transition-duration: var(--hover-time);
+  transition-property: backdrop-filter, width, height;
   position: absolute;
   width: 100%;
   height: 100%;
   font-weight: bold;
   align-items: center;
   justify-content: center;
+}
+
+.image-title {
+  font-size: 1rem;
+  display: flex;
+  justify-content: center;
+  position: relative;
+  color: var(--text-accent-color);
+  transition: font-size var(--hover-time);
+
+  &::after {
+    transition-duration: var(--hover-time);
+    transition-property: width;
+    display: block;
+    content: "";
+    position: absolute;
+    height: var(--underline-size);
+    width: 0;
+    bottom: calc(-1 * var(--underline-size) + var(--underline-offset-size));
+    background-color: var(--text-accent-color);
+  }
 }
 
 .image-link {
--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -3,11 +3,11 @@ import { ref } from "vue";
 import ImageHoverCollectionHorizontal from "../ImageSelectorMenuHorizontal.vue";
 
 const count = ref(0);
-const exampleImages = [
+const minecraftProjects = [
   {
     title: "Apollo",
     alt: "Apollo logo",
-    src: "https://cdn.modrinth.com/data/rQU8oPFc/icon.png",
+    src: "https://media.forgecdn.net/avatars/478/208/637773649648384039.png",
     route: "projects/apollo",
   },
   {
@@ -17,34 +17,34 @@ const exampleImages = [
     route: "projects/classic-3d",
   },
   {
-    title: "Classic 3D",
-    alt: "Classic 3D logo",
-    src: "https://cdn.modrinth.com/data/FRSckbRo/icon.png",
+    title: "Commands+",
+    alt: "",
+    src: "https://media.forgecdn.net/avatars/569/858/637930634199896245.png",
     route: "projects/classic-3d",
   },
   {
-    title: "Classic 3D",
-    alt: "Classic 3D logo",
-    src: "https://cdn.modrinth.com/data/FRSckbRo/icon.png",
-    route: "projects/classic-3d",
+    title: "Title",
+    alt: "",
+    src: "https://picsum.photos/seed/dasj/1920/1080",
+    route: "",
   },
   {
-    title: "Classic 3D",
-    alt: "Classic 3D logo",
-    src: "https://cdn.modrinth.com/data/FRSckbRo/icon.png",
-    route: "projects/classic-3d",
+    title: "SUPER LONG TITLE TO TRY AND OVERFLOW",
+    alt: "",
+    src: "https://picsum.photos/seed/jgnjk/1920/1080",
+    route: "",
   },
   {
-    title: "Classic 3D",
-    alt: "Classic 3D logo",
-    src: "https://cdn.modrinth.com/data/FRSckbRo/icon.png",
-    route: "projects/classic-3d",
+    title: "Medium title this time",
+    alt: "",
+    src: "https://picsum.photos/seed/gjnfjkng/1920/1080",
+    route: "",
   },
   {
-    title: "Classic 3D",
-    alt: "Classic 3D logo",
-    src: "https://cdn.modrinth.com/data/FRSckbRo/icon.png",
-    route: "projects/classic-3d",
+    title: "hi",
+    alt: "",
+    src: "https://picsum.photos/seed/ginmn/1920/1080",
+    route: "",
   },
 ];
 </script>
@@ -55,7 +55,7 @@ const exampleImages = [
     <button @click="count++">Counter: {{ count }}</button>
     <p>Here is an <RouterLink to="/">inline link</RouterLink>
     </p>
-    <h1>Minecraft</h1>
-    <ImageHoverCollectionHorizontal :images="exampleImages" />
+    <h1>Minecraft Projects</h1>
+    <ImageHoverCollectionHorizontal :images="minecraftProjects" />
   </div>
 </template>
