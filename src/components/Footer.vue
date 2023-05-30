151a0c0425be26ef2324b43277ecd2e87387a1af
Ryan Garrett <RyanGar46@gmail.com>
2023-05-29 21:24:19 -0400

    Update username

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,9 @@
 <template>
   <footer>
     <p>Created by: Ryan Garrett (ThePinkHacker)</p>
+    <p>Contact me at: <a href="mailto:ryan@thepinkhacker.com">ryan@thepinkhacker.com</a></p>
     <p>Hosted on: <a href="https://firebase.google.com/">Firebase</a></p>
-    <p>Source code hosted on: <a href="https://github.com/RyanGar46/thepinkhacker.com">GitHub</a></p>
+    <p>Source code hosted on: <a href="https://github.com/RyanThePinkHacker/thepinkhacker.com">GitHub</a></p>
   </footer>
 </template>
 
