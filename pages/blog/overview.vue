8f633433c01019113d970b5db9ad16cd039bcea0
Ryan Garrett <ryan@thepinkhacker.com>
2024-03-12 21:04:40 -0400

    Properly sort content query

--- pages/blog/overview.vue
+++ pages/blog/overview.vue
@@ -1,8 +1,9 @@
 <template>
     <h1>Welcome To My Blog</h1>
     <div class="blog-posts">
-        <ContentQuery :only="['_path', 'title', 'description', '_draft', 'date']" v-slot="{ data }">
-            <template v-for="{ _path, title, description, _draft, date } in data.reverse()">
+        <ContentQuery :only="['_path', 'title', 'description', '_draft', 'date']" v-slot="{ data }"
+            :sort="{ title: -1 }">
+            <template v-for="{ _path, title, description, _draft, date } in data">
                 <BlogPostCard v-if="!_draft" :path="_path" :title="title" :description="description" :date="date" />
                 <DevOnly v-else>
                     <BlogPostCard :path="_path" :title="title" :description="description" :date="date" />
