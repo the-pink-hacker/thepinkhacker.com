bf256405fa62ebda3b8ee8ea8b5729ac5528c9f9
Ryan Garrett <ryan@thepinkhacker.com>
2024-03-18 20:13:35 -0400

    Improve browser scaling

--- app.vue
+++ app.vue
@@ -1,41 +1,51 @@
 <script lang="ts" setup>
 useHead({
-  htmlAttrs: {
-    lang: "en"
-  },
-  link: [
-    {
-      rel: "icon",
-      type: "image/x-icon",
-      href: "/favicon.ico",
+    htmlAttrs: {
+        lang: "en"
     },
-    {
-      rel: "me",
-      href: "https://social.linux.pizza/@ThePinkHacker",
-    },
-    {
-      rel: "stylesheet",
-      href: "https://fonts.googleapis.com/icon?family=Material+Icons",
-    }
-  ],
-  meta: [
-    {
-      name: "viewport",
-      content: "width=device-width; user-scalable=yes",
-    },
-  ],
+    link: [
+        {
+            rel: "icon",
+            type: "image/x-icon",
+            href: "/favicon.ico",
+        },
+        {
+            rel: "me",
+            href: "https://social.linux.pizza/@ThePinkHacker",
+        },
+        {
+            rel: "stylesheet",
+            href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        }
+    ],
+    meta: [
+        {
+            name: "viewport",
+            content: "width=device-width; user-scalable=yes",
+        },
+    ],
+});
+
+const scale = ref(1);
+
+onMounted(() => {
+    window.visualViewport?.addEventListener("resize", () => {
+        scale.value = devicePixelRatio;
+    });
 });
 </script>
 
 <template>
-  <NavBar />
-  <main>
-    <NuxtPage />
-  </main>
-  <Footer />
+    <div :style="`font-size: ${scale}rem`">
+        <NavBar />
+        <main>
+            <NuxtPage />
+        </main>
+        <Footer />
 
-  <Head>
-    <Link rel="canonical" :href="'https://thepinkhacker.com' + $route.path" />
-    <Meta property="og:url" :content="'https://thepinkhacker.com' + $route.path" />
-  </Head>
+        <Head>
+            <Link rel="canonical" :href="'https://thepinkhacker.com' + $route.path" />
+            <Meta property="og:url" :content="'https://thepinkhacker.com' + $route.path" />
+        </Head>
+    </div>
 </template>
