7b95ae44ec5548b5ee09d995b1895c9fded649ad
Pink Garrett <pink@thepinkhacker.com>
2024-04-29 21:52:24 -0400

    Setup RSS for blog

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: {
-    enabled: true
-  },
-  css: [
-    "~/assets/style/main.scss",
-  ],
-  modules: [
-    "@nuxt/content",
-  ],
-  content: {
-    highlight: {
-      theme: "github-dark",
-      preload: [
-        "rust",
-        "toml",
-        "asm",
-      ],
+    devtools: {
+        enabled: true
     },
-  },
+    css: [
+        "~/assets/style/main.scss",
+    ],
+    modules: [
+        "@nuxt/content",
+    ],
+    content: {
+        highlight: {
+            theme: "github-dark",
+            preload: [
+                "rust",
+                "toml",
+                "asm",
+            ],
+        },
+    },
 });
---
+++ server/routes/blog/rss.ts
@@ -0,0 +1,31 @@
+import RSS from "rss";
+import { serverQueryContent } from "#content/server";
+
+export default defineCachedEventHandler(async (event) => {
+    const posts = await serverQueryContent(event, "/blog")
+        .only(["title", "description", "date", "_draft", "_path"])
+        .find();
+
+    const feed = new RSS({
+        title: "Pink's Blog",
+        description: "My personal blog about whatever projects I'm working on at the moment.",
+        site_url: "https://thepinkhacker.com/blog",
+        feed_url: "https://thepinkhacker.com/blog/rss",
+    });
+
+    for (const { description, title, date, _draft, _path } of posts) {
+        if (_draft) continue;
+
+        feed.item({
+            title: title ?? "Untitled Post",
+            url: `https://thepinkhacker.com${_path}`,
+            date,
+            description,
+        });
+    }
+
+    const output = feed.xml({ indent: true });
+
+    event.node.res.setHeader("content-type", "text/xml");
+    event.node.res.end(output);
+});
