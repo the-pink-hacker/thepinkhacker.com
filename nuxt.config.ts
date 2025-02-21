1a1ee1219daac5642b36b0c7201bf3aa8414336f
Pink Garrett <pink@thepinkhacker.com>
2025-02-20 23:03:11 -0500

    Update nuxt config compatibility

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,46 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: {
-        enabled: true
-    },
-    css: [
-        "~/assets/style/main.scss",
-    ],
-    modules: [
-        "@nuxt/content",
-        "@nuxtjs/sitemap",
-    ],
-    content: {
-        highlight: {
-            theme: "github-dark",
-            preload: [
-                "rust",
-                "toml",
-                "asm",
-                "typescript",
-                "c",
-                "c++",
-                "hjson",
-            ],
-        },
-    },
-    site: {
-        url: "https://thepinkhacker.com",
-    },
-    sitemap: {
-        sources: [
-            "/api/__sitemap__/urls"
-        ],
-    },
-    nitro: {
-        compressPublicAssets: true,
-    },
+  devtools: {
+      enabled: true
+  },
+
+  css: [
+      "~/assets/style/main.scss",
+  ],
+
+  modules: [
+      "@nuxt/content",
+      "@nuxtjs/sitemap",
+  ],
+
+  content: {
+      highlight: {
+          theme: "github-dark",
+          preload: [
+              "rust",
+              "toml",
+              "asm",
+              "typescript",
+              "c",
+              "c++",
+              "hjson",
+          ],
+      },
+  },
+
+  site: {
+      url: "https://thepinkhacker.com",
+  },
+
+  sitemap: {
+      sources: [
+          "/api/__sitemap__/urls"
+      ],
+  },
+
+  nitro: {
+      compressPublicAssets: true,
+  },
+
+  compatibilityDate: "2025-02-20",
 });
